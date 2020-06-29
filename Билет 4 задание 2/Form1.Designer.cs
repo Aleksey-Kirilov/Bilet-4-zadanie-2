8d9e684c9865d42136c0a86044c0ec49ab693d28
Aleksey-Kirilov <aleks.kirilov2015@yandex.ru>
2020-06-29 10:47:47 +0500

    Добавьте файлы проекта.

---
+++ Билет 4 задание 2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Билет_4_задание_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtBx
+            // 
+            this.TxtBx.Location = new System.Drawing.Point(161, 106);
+            this.TxtBx.Name = "TxtBx";
+            this.TxtBx.Size = new System.Drawing.Size(253, 20);
+            this.TxtBx.TabIndex = 0;
+            this.TxtBx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Проверьте, можно ли ввести дробное число";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 196);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtBx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBx;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
